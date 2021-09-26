0cfa2a2f15f06a8d40cb8f23a7d6772fa30aa2e6
Mogakamo <mogaka.amo254@gmail.com>
2021-09-26 14:19:20 +0000

    commit

--- gee-maps/src/App.vue
+++ gee-maps/src/App.vue
@@ -1,15 +1,16 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <GoogleMaps />
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import GoogleMaps from "./components/GoogleMaps.vue"
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    GoogleMaps,
   }
 }
 </script>
--- gee-maps/src/main.js
+++ gee-maps/src/main.js
@@ -1,4 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
+import * as VueGoogleMaps from ""
+
+
 createApp(App).mount('#app')
