ba11ecc3a668a046e2f214872e10d9cd5505b60a
Mogakamo <mogaka.amo254@gmail.com>
2021-09-26 14:36:01 +0000

    commit

--- gee-maps/src/main.js
+++ gee-maps/src/main.js
@@ -1,7 +1,18 @@
-import { createApp } from 'vue'
 import App from './App.vue'
 
-import * as VueGoogleMaps from ""
+import * as VueGoogleMaps from "vue2-google-maps"
+import Vue from 'vue'
+
+Vue.config.productionTip = false;
+
+Vue.use(VueGoogleMaps, {
+  load: {
+    key: "AIzaSyAFpCdi4OzvUUr9VYwayGr1URY3FXWB3bs",
+    libraries: "places",
+  },
+});
 
 
-createApp(App).mount('#app')
+new Vue({
+  render: (h) => h(App),
+}).$mount("#app");
