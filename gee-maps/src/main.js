d50f071bef2a4a5c5118273689cc212a3303cb5c
Mogakamo <mogaka.amo254@gmail.com>
2021-09-26 16:01:01 +0000

    commit

--- gee-maps/src/main.js
+++ gee-maps/src/main.js
@@ -1,7 +1,19 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueGoogleMap from "vuejs-google-maps";
+import "vuejs-google-maps/dist/vuejs-google-maps.css";
 
 Vue.config.productionTip = false
+
+Vue.use(VueGoogleMap, {
+  load: {
+    apiKey: "AIzaSyAFpCdi4OzvUUr9VYwayGr1URY3FXWB3bs",
+    libraries: [
+      /* rest of libraries */
+      
+    ],
+  },
+});
 
 new Vue({
   render: h => h(App),
