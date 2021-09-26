398ea6e482d0d0d69afc7a63e7a1ba7fcac0a1ff
Mogakamo <mogaka.amo254@gmail.com>
2021-09-26 11:50:40 +0000

    added the credentials

--- gee-maps/src/main.js
+++ gee-maps/src/main.js
@@ -4,4 +4,14 @@ import "./registerServiceWorker";
 import router from "./router";
 import store from "./store";
 
+//vue-maps
+import * as VueGoogleMaps from 'vue2-google-maps'
+
+VueGoogleMaps.use(VueGoogleMaps, {
+  load: {
+    key: "AIzaSyAFpCdi4OzvUUr9VYwayGr1URY3FXWB3bs",
+    libraries: "places",
+  },
+});
+
 createApp(App).use(store).use(router).mount("#app");
