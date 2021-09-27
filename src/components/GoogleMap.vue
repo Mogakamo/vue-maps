3f70ad07697e8bfcf9dbeef7c4612385b9524f5b
Mogakamo <mogaka.amo254@gmail.com>
2021-09-27 07:06:24 +0000

    commit

---
+++ src/components/GoogleMap.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    Hello Google Maps
+    <GmapMap
+  :center="{lat:10, lng:10}"
+  :zoom="7"
+  map-type-id="terrain"
+  style="width: 100%; height: 100vh"
+>
+</GmapMap>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'GoogleMap',
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,22 @@
 import Vue from 'vue'
 import App from './App.vue'
+import * as VueGoogleMaps from 'vue2-google-maps'
+
 
 Vue.config.productionTip = false
+
+Vue.use(VueGoogleMaps, {
+  load: {
+    key: 'AIzaSyD18SXyA13TP-Hlsj0c6_qOgRJH3sWgl44',
+    libraries: 'places', // This is required if you use the Autocomplete plugin
+    // OR: libraries: 'places,drawing'
+    // OR: libraries: 'places,drawing,visualization'
+    // (as you require)
+ 
+    //// If you want to set the version, you can do so:
+    // v: '3.26',
+  },
+})
 
 new Vue({
   render: function (h) { return h(App) },
